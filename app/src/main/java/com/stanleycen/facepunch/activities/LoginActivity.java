2f3171e9784462ba6b53744611ad51857cc03e9a
Stanley Cen <jiecenzhao@gmail.com>
2014-02-11 19:35:14 -0800

    Install hugo, androidannotations, and eventbus

---
+++ app/src/main/java/com/stanleycen/facepunch/activities/LoginActivity.java
@@ -0,0 +1,7 @@
+package com.stanleycen.facepunch.activities;
+
+import android.app.Activity;
+
+public class LoginActivity extends Activity {
+
+}
---
+++ app/src/main/java/com/stanleycen/facepunch/activities/MainActivity.java
@@ -0,0 +1,7 @@
+package com.stanleycen.facepunch.activities;
+
+/**
+ * Created by scen on 2/11/14.
+ */
+public class MainActivity {
+}
