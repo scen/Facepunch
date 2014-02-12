bc8929dce0e91cbb024d8ca5f969f42081e44a6f
Stanley Cen <jiecenzhao@gmail.com>
2014-02-11 20:09:00 -0800

    Test annotations

--- app/src/main/java/com/stanleycen/facepunch/activities/LoginActivity.java
+++ app/src/main/java/com/stanleycen/facepunch/activities/LoginActivity.java
@@ -2,6 +2,11 @@ package com.stanleycen.facepunch.activities;
 
 import android.app.Activity;
 
+import com.stanleycen.facepunch.R;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
 public class LoginActivity extends Activity {
 
 }
